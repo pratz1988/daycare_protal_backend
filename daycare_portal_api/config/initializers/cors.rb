c766026b3ddfd381833cca53f05436b6209003d0
pratz1988 <prathyusha.jaddu@gmail.com>
2019-12-19 12:10:58 -0500

    fixed seed data

--- daycare_portal_api/config/initializers/cors.rb
+++ daycare_portal_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://daycare-portal.surge.sh'
+    origins 'https://daycare-portal.surge.sh/'
 
     resource '*',
       headers: :any,
